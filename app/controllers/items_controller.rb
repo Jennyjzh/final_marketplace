1a39ff594a1477d719def94c07693fc2294d2a64
Raghu Betina <raghu@firstdraft.com>
2018-12-04 21:27:48 +0000

    Only item user can modify item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :current_user_must_be_item_poster, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_item_poster
+    item = Item.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == item.poster
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @items = Item.all
 
